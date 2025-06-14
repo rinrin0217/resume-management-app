a7bfa883368e7f04bec8e94a5c6b3c451432844d
Takeyama <wushanlintai@gmai.com>
2025-06-14 18:03:41 +0900

    タスク1が完了しました

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
-import Image from "next/image";
+import Header from "../components/Header";
+import Section from "../components/Section";
 
-export default function Home() {
+export default function Page() {
   return (
-    <h1>Hello</h1>
+    <>
+      <Header />
+      <Section />
+    </>
   );
 }
