3954d41cb2770f09d34be1064e984432d10d2d0f
Takeyama <wushanlintai@gmai.com>
2025-06-14 18:27:15 +0900

    Initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
-import Header from "../components/Header";
-import Section from "../components/Section";
-
 export default function Page() {
   return (
     <>
-      <Header />
-      <Section />
+      <h1>Hello</h1>
     </>
   );
 }
